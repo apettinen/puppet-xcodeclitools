556aaeb9142d4c03e94c48bdb9269becaf4034d0
Antti Pettinen <antti.pettinen@tut.fi>
2016-05-10 14:09:06 +0300

    now with YUGE sed /AP

--- files/install_xcode_cli_tools.sh
+++ files/install_xcode_cli_tools.sh
@@ -5,6 +5,6 @@
 #https://github.com/timsutton/osx-vm-templates/blob/master/scripts/xcode-cli-tools.sh
 # Copyright: Tim Sutton
 
-PROD=$(/usr/sbin/softwareupdate -l | /usr/bin/sed '/\* Command Line Tools/!d;s/*//;s/^[ \t]*//;s/[ \t]*$//;s/\n/ /g')
+PROD=$(/usr/sbin/softwareupdate -l | /usr/bin/sed '/\* Command Line Tools/!d;s/*//;s/^[ \t]*//;s/[ \t]*$//;s/\n//g;q')
 # install it
 /usr/sbin/softwareupdate -i "$PROD" -v
