b70c5256bfc93f4d768d25836e7d6d212a5fbf5b
Antti Pettinen <antti.pettinen@tut.fi>
2016-05-11 12:13:17 +0300

    Commit by: AP - Initial fully working version - cleaned up the code a bit

--- manifests/remove_helpers.pp
+++ manifests/remove_helpers.pp
@@ -18,26 +18,13 @@ define xcodeclitools::remove_helpers (
   String $script_dir = $xcodeclitools::xcode_install_script_dir
   ) {
   validate_string($script_dir)
-  # file { 'remove_installondemand':
-  #   ensure => absent,
-  #   path   =>
-  #   '/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress',
-  #   # subscribe => Exec['install_Xcode_CLI_Tools'],
-  # }
-  #
-  # file { 'remove_xcode_cli_install_script':
-  #   ensure => absent,
-  #   path   => "${script_dir}/install_xcode_cli_tools.sh",
-  #   # subscribe => Exec['install_Xcode_CLI_Tools'],
-  # }
+
   exec { 'remove_installondemand':
     command => 'rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress',
     path    => '/bin',
-    #require =>  File['xcodeclitools::set_installondemand']
   }
   exec { 'remove_xcode_cli_install_script':
     command => "rm ${script_dir}/install_xcode_cli_tools.sh",
     path    => '/bin',
-    # refreshonly => true,
   }
 }
